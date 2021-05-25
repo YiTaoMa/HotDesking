46ebfbb0eb1ec7eef1d550fcd487c6c1648be4fe
Yitao Ma(s3843689) <s3843689@student.rmit.edu.au>
2021-05-25 10:21:47 +1000

    Upload time validation for admin
    Upload function time validation for admin confirm booking. Now if any tomorrow booking not confirmed by midnight before tomorrow will automate cancel.

--- src/main/controller/MainAdminController.java
+++ src/main/controller/MainAdminController.java
@@ -3,17 +3,72 @@ package main.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import main.model.CancelBookingConfirmModel;
+import main.model.MainAdminModel;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.ResourceBundle;
 
-public class MainAdminController {
+public class MainAdminController implements Initializable {
+    MainAdminModel mainAdminModel = new MainAdminModel();
+    //HashMap<Integer, String> datesAndNumberFromBookingNotConfirmed = new HashMap<>();
+    HashMap<Integer, String> bookingsOutOfRange = new HashMap<>();
+
+    LinkedList<Integer> bookingsOutOfRangeEmpId = new LinkedList<>();
+    LinkedList<String> bookingsOutOfRangeDate = new LinkedList<>();
     @FXML
     private BorderPane borderPaneMainAdmin;
+
+    /**
+     * Every time this scene is shown, which right before admin select bookings to manage, time validation framework here
+     * will automate delete bookings and their whitelist if any tomorrow booking has not been confirmed by midnight before tomorrow
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // clear everything at the start
+        bookingsOutOfRange.clear();
+        bookingsOutOfRangeEmpId.clear();
+        bookingsOutOfRangeDate.clear();
+
+        try {
+            bookingsOutOfRange = mainAdminModel.getBookingsOutOfRanges(); // get all booking out of ranges (need to delete)
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        for (Entry<Integer, String> entry : bookingsOutOfRange.entrySet()) { // for each out of ranges booking
+            mainAdminModel.assignOutOfRangeEmpIDDateToCollection(entry.getKey()); //assign their emp ID and date to the model
+            //2 linked list used to delete their corresponding whitelist
+        }
+        // after loop, after all assigned, we assign the 2 linked list in model to the controller to used it
+        bookingsOutOfRangeEmpId = mainAdminModel.getBookingsOutOfRangeEmpIdCollection();
+        bookingsOutOfRangeDate = mainAdminModel.getBookingsOutOfRangeDateCollection();
+
+        for (Entry<Integer, String> entry : bookingsOutOfRange.entrySet()) { // for each out of ranges booking
+            //System.out.println(entry.getKey());
+            mainAdminModel.deleteOutOfRangeBookings(entry.getKey());// we delete them through their unique booking number
+        }
+
+        for (int i = 0; i < bookingsOutOfRangeEmpId.size(); i++) { // as these 2 collection size should be same, so use 1 of them will be fine
+            //System.out.println(bookingsOutOfRangeEmpId.get(i) + "---"+bookingsOutOfRangeDate.get(i));
+            mainAdminModel.deleteOutOfRangeWhitelistRecord(bookingsOutOfRangeEmpId.get(i), bookingsOutOfRangeDate.get(i));
+            // delete their whitelist record by pass their emp ID and date, date plus 1 day in the model
+        }
+        mainAdminModel.clearEmpIdDateCollection();// clear 2 linked list in the model method after we done the job.
+
+    }
 
     public void switchToManageBooking(ActionEvent event) {
         Scene scene = borderPaneMainAdmin.getScene();
@@ -27,4 +82,16 @@ public class MainAdminController {
             System.out.println("Cannot load the adminSelectBookingToManage.fxml");
         }
     }
+
+    //public void testHashMap(){
+    //    System.out.println("Booking not confirmed");
+    //    System.out.println(mainAdminModel.getBookingEmpIDAndDatesNotConfirmed());
+    //    System.out.println("Bookings need to delete");
+    //    try {
+    //        System.out.println(mainAdminModel.getBookingsOutOfRanges());
+    //    }
+    //    catch (ParseException e){
+    //        e.printStackTrace();
+    //    }
+    //}
 }
---
+++ src/main/model/MainAdminModel.java
@@ -0,0 +1,159 @@
+package main.model;
+
+import main.DBUtils;
+import main.SQLConnection;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.Date;
+import java.util.Map.Entry;
+
+public class MainAdminModel {
+    HashMap<Integer, String> datesAndNumberFromBookingNotConfirmed = new HashMap<>();
+    HashMap<Integer, String> bookingsOutOfRange = new HashMap<>();
+    LinkedList<Integer> bookingsOutOfRangeEmpId = new LinkedList<>();
+    LinkedList<String> bookingsOutOfRangeDate = new LinkedList<>();
+
+
+    public void getBookingNumberAndDatesNotConfirmed() { // as we need to get unconfirmed bookings first then check date and time
+        datesAndNumberFromBookingNotConfirmed.clear();
+        Connection connection;
+        connection = SQLConnection.connect();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        String query = "select number,date from Booking where has_confirmed=false";
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            while (resultSet.next()) { // for each result we put key and their value to hash map
+                datesAndNumberFromBookingNotConfirmed.put(resultSet.getInt("number"), resultSet.getString("date"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtils.closeResultSet(resultSet);
+            DBUtils.closeStatement(statement);
+            DBUtils.closeConnection(connection);
+        }
+    }
+
+    public HashMap<Integer, String> getBookingsOutOfRanges() throws ParseException {
+        // call this method directly to assign un confirmed booking to the hash map
+        getBookingNumberAndDatesNotConfirmed();
+        bookingsOutOfRange.clear();
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
+        Date nowTime = new Date(); // today
+        Date bookingTime; // target booking date
+        boolean effectiveDate;
+        for (Entry<Integer, String> entry : datesAndNumberFromBookingNotConfirmed.entrySet()) { // for each un confirmed booking
+            bookingTime = ft.parse(entry.getValue());// get target booking date which is the value of each entry/booking
+            effectiveDate = isEffectiveDate(nowTime, bookingTime);// we pass today and target date to compare,and get the result
+            if (!effectiveDate) { // if the date is out of ranges
+                bookingsOutOfRange.put(entry.getKey(), entry.getValue()); // we put those bookings into this hash map as the key
+                // is booking number and value is date
+            }
+        }
+        return bookingsOutOfRange;
+    }
+
+    public boolean isEffectiveDate(Date nowTime, Date bookingTime) { // check if date is valid
+
+        Calendar todayCalendar = Calendar.getInstance();
+        todayCalendar.setTime(nowTime);
+
+        Calendar bookingDate = Calendar.getInstance();
+        bookingDate.setTime(bookingTime);
+        // if today is before the target date/booking date in the database, which means before today's 12:00pm right before tomorrow
+        // we can still confirm it. if we pass today's 12:00pm which is actually tomorrow, then today becomes tomorrow and will fail
+        // which will return false
+
+        // Also, if we have date less than today, which today is not before let's say last date, it still return false.
+        if (todayCalendar.before(bookingDate)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void deleteOutOfRangeBookings(int bookingNumber) { // delete bookings which is out of ranges through booking number
+        Connection connection;
+        connection = SQLConnection.connect();
+        String query = "delete from Booking where number=?";
+        PreparedStatement prst = null;
+        try {
+            prst = connection.prepareStatement(query); // PS to SQL statement
+            prst.setInt(1, bookingNumber);
+            prst.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtils.closePrepareStatement(prst);
+            DBUtils.closeConnection(connection);
+        }
+    }
+
+    public void assignOutOfRangeEmpIDDateToCollection(int bookingNumber) { // get corresponding emp ID and date for those bookings
+        //out of ranges
+        Connection connection;
+        connection = SQLConnection.connect();
+        String query = "select employee_id,date from Booking where number=?"; // we through number to get employee id and date
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, bookingNumber);
+
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) { // assign each result to corresponding emp ID linked list and date linked list
+                bookingsOutOfRangeEmpId.add(resultSet.getInt("employee_id"));
+                bookingsOutOfRangeDate.add(resultSet.getString("date"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtils.closeResultSet(resultSet);
+            DBUtils.closePrepareStatement(preparedStatement);
+            DBUtils.closeConnection(connection);
+        }
+    }
+
+    public void deleteOutOfRangeWhitelistRecord(int empId, String date) { // delete corresponding whitelist record
+        Connection connection;
+        connection = SQLConnection.connect();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+        LocalDate dateConverted = LocalDate.parse(date, formatter);
+        dateConverted = dateConverted.plusDays(1);//add one day if it is whitelist
+        String dateString = dateConverted.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        String query = "delete from Whitelist where employee_id=? and date=?";
+        PreparedStatement prst = null;
+        try {
+            prst = connection.prepareStatement(query); // PS to SQL statement
+            prst.setInt(1, empId);
+            prst.setString(2, dateString);
+            prst.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtils.closePrepareStatement(prst);
+            DBUtils.closeConnection(connection);
+        }
+    }
+
+    public void clearEmpIdDateCollection() { // clear everything in these 2 linked list
+        bookingsOutOfRangeEmpId.clear();
+        bookingsOutOfRangeDate.clear();
+    }
+
+    public LinkedList<Integer> getBookingsOutOfRangeEmpIdCollection() { // get this linked list
+        return bookingsOutOfRangeEmpId;
+    }
+
+    public LinkedList<String> getBookingsOutOfRangeDateCollection() { // get this linked list
+        return bookingsOutOfRangeDate;
+    }
+}
